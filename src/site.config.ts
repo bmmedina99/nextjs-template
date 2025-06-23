63affea99e2f543c62cce8844a868165051b3a06
bmmedina99 <99305658+bmmedina99@users.noreply.github.com>
2025-06-23 23:30:57 +0200

    feat: :wrench: add src/site.config.ts with types

---
+++ src/site.config.ts
@@ -0,0 +1,10 @@
+import type { Config } from './types'
+
+export const configSite: Config = {
+  title: 'NextKit',
+  description:
+    'Plantilla de inicio para proyectos con Next.js App Router, preconfigurada con TypeScript y Tailwind CSS.',
+  lang: 'es',
+  author: 'bmmedina99',
+  url: 'https://localhost:5000',
+}
