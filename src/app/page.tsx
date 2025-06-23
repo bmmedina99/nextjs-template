cdbc91a8ff49e1816707c7e93b1d27ef1ddc718c
bmmedina99 <99305658+bmmedina99@users.noreply.github.com>
2025-06-23 23:47:58 +0200

    refactor(app): :recycle: restructure app folder for dynamic projects

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
-import Footer from '@/components/Footer'
-import Header from '@/components/Header'
-import Main from '@/components/Main'
+import Footer from '@/components/layouts/Footer'
+import Header from '@/components/layouts/Header'
+import Main from '@/components/layouts/Main'
 
 export default function Home() {
   return (
