d14f331f5820bb6b390b58c762e2254a9cad00c2
bmmedina99 <99305658+bmmedina99@users.noreply.github.com>
2025-09-11 22:33:16 +0200

    style(font): :art: add Poppins font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,12 @@
+import { Poppins } from 'next/font/google'
 import { baseMetadata } from '@/components/metadata.config'
 import { configSite } from '@/site.config'
+
+const poppins = Poppins({
+  subsets: ['latin'],
+  weight: ['400', '600', '700'],
+  display: 'swap',
+})
 
 export const metadata = baseMetadata
 
@@ -9,7 +16,7 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang={configSite.lang}>
+    <html lang={configSite.lang} className={poppins.className}>
       <body className='bg-gradient-to-br from-stone-950 via-slate-600 to-stone-950'>
         {children}
       </body>
