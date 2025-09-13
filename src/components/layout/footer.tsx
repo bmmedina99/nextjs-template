335bdbe22282cc2b440ebcd43fd745f3518f0891
bmmedina99 <99305658+bmmedina99@users.noreply.github.com>
2025-09-13 23:28:52 +0200

    style(footer): :art: add text-center in footer component

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -2,36 +2,33 @@ import Icon from '../ui/icon'
 
 const Footer = () => {
   return (
-    <footer className='px-4 py-12 bg-slate-900'>
-      <div className='container mx-auto'>
-        <p className='mb-4 text-xs text-center text-gray-300 md:text-sm'>
-          src/components/layout/footer.tsx
-        </p>
-        <div className='flex flex-col items-center justify-between md:flex-row'>
-          <div className='flex items-center mb-4 space-x-2 md:mb-0'>
-            <div className='flex items-center justify-center w-6 h-6 rounded bg-gradient-to-r from-brandels-blue to-aquamarine'>
-              <Icon
-                name='nextjs'
-                width={12}
-                height={12}
-                classes='text-stone-950'
-              />
-            </div>
-            <span className='font-semibold text-white'>NextKit Template</span>
+    <footer className='px-4 py-12 text-center bg-slate-900'>
+      <p className='mb-4 text-xs text-center text-gray-300 md:text-sm'>
+        src/components/layout/footer.tsx
+      </p>
+      <section className='flex flex-col items-center justify-between md:flex-row'>
+        <div className='flex items-center mb-4 space-x-2 text-white md:mb-0'>
+          <div className='flex items-center justify-center rounded size-6 bg-gradient-to-r from-brandels-blue to-aquamarine'>
+            <Icon
+              name='nextjs'
+              width={12}
+              height={12}
+            />
           </div>
-          <p className='text-sm text-slate-400'>
-            {new Date().getFullYear()} | Desarrollador y diseñado por{' '}
-            <a
-              href='https://bmmedina.dev'
-              target='_blank'
-              rel='noopener noreferrer'
-              className='underline transition-colors hover:text-brandels-blue text-sky-400'
-            >
-              @bmmedina99
-            </a>
-          </p>
+          <span className='font-semibold'>NextKit Template</span>
         </div>
-      </div>
+        <p className='text-sm text-slate-300'>
+          {new Date().getFullYear()} | Desarrollador y diseñado por{' '}
+          <a
+            href='https://bmmedina.dev'
+            target='_blank'
+            rel='noopener noreferrer'
+            className='underline transition-colors hover:text-brandels-blue text-sky-400'
+          >
+            @bmmedina99
+          </a>
+        </p>
+      </section>
     </footer>
   )
 }
