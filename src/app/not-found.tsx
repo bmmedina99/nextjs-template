e450765aceb8732db0ec6246103d8ad33a839165
bmmedina99 <99305658+bmmedina99@users.noreply.github.com>
2025-09-13 23:23:51 +0200

    fix(ui): :bug: correct design issue in not-found.tsx

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,37 +1,35 @@
 'use client'
 
+import Main from '@/components/layout/main'
+
 export default function notFound() {
   return (
-    <div className='relative flex items-center justify-center min-h-screen p-4 overflow-hidden'>
-      <div className='relative z-10 w-full max-w-4xl space-y-12 text-center'>
+    <Main className='flex items-center justify-center flex-grow overflow-hidden'>
+      <div className='relative z-10 max-w-4xl mx-auto space-y-4 text-center'>
         <p className='mb-4 text-xs text-gray-300 md:text-sm'>
           src/app/not-found.tsx
         </p>
-        <div className='text-6xl sm:text-8xl font-bold leading-none'>
-          <span className='text-transparent bg-gradient-to-r from-brandels-blue via-aquamarine to-brandels-blue bg-clip-text'>
-            404
-          </span>
-        </div>
-        <div className='space-y-6'>
-          <h1 className='text-4xl font-bold text-gray-500 sm:text-5xl'>
-            Página no encontrada
-          </h1>
-          <p className='max-w-2xl mx-auto text-xl text-gray-200'>
+        <p className='text-6xl font-bold leading-none text-transparent sm:text-8xl bg-clip-text bg-gradient-to-t from-brandels-blue via-aquamarine to-brandels-blue'>
+          404
+        </p>
+        <section className='space-y-6'>
+          <h1 className='font-bold text-white'>Página no encontrada</h1>
+          <p className='max-w-2xl mx-auto text-xl leading-relaxed text-slate-300 text-pretty'>
             Lo sentimos, la página que estás buscando no existe o ha sido
             movida. Pero no te preocupes, hay muchas otras cosas increíbles que
             puedes explorar.
           </p>
-        </div>
+        </section>
         <div className='flex items-center justify-center'>
           <button
             type='button'
             onClick={() => window.history.back()}
-            className='flex items-center justify-center gap-4 px-4 py-3 text-white border-0 rounded-md bg-gradient-to-r from-blue-500 to-purple-600 hover:from-blue-600 hover:to-purple-700'
+            className='flex gap-4 px-4 py-3 text-white border-0 rounded-md bg-gradient-to-r from-blue-500 to-purple-600 hover:from-blue-600 hover:to-purple-700'
           >
             Volver a la página anterior
           </button>
         </div>
       </div>
-    </div>
+    </Main>
   )
 }
