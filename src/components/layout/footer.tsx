dab09ae107bfbb286d51b749c45e70349f4a1ef5
bmmedina99 <99305658+bmmedina99@users.noreply.github.com>
2025-11-18 23:43:41 +0100

    style: :art: refine footer layout and spacing for improved readability

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -2,13 +2,13 @@ import Icon from '../ui/icon'
 
 const Footer = () => {
   return (
-    <footer className='px-4 py-12 text-center bg-slate-900'>
-      <p className='mb-4 text-xs text-center text-gray-300 md:text-sm'>
+    <footer className='px-4 py-12 bg-slate-900 text-center'>
+      <p className='mb-4 text-xs text-gray-300 md:text-sm'>
         src/components/layout/footer.tsx
       </p>
-      <section className='flex flex-col items-center justify-between md:flex-row'>
+      <section className='container mx-auto flex flex-col items-center justify-between md:flex-row'>
         <div className='flex items-center mb-4 space-x-2 text-white md:mb-0'>
-          <div className='flex items-center justify-center rounded size-6 bg-linear-to-r from-brandels-blue to-aquamarine'>
+          <div className='flex items-center justify-center size-6 rounded bg-linear-to-r from-brandels-blue to-aquamarine'>
             <Icon
               name='nextjs'
               width={12}
@@ -18,7 +18,7 @@ const Footer = () => {
           <span className='font-semibold'>NextKit Template</span>
         </div>
         <p className='text-sm text-slate-300'>
-          {new Date().getFullYear()} | Desarrollador y diseñado por{' '}
+          {new Date().getFullYear()} | Desarrollador y diseñado por
           <a
             href='https://bmmedina.dev'
             target='_blank'
