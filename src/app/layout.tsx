710a87c202e4152956859005959eeed425a3705c
bmmedina99 <99305658+bmmedina99@users.noreply.github.com>
2026-04-11 02:10:53 +0200

    refactor(styles): move body classes to global CSS

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
       lang={configSite.lang}
       className={poppins.className}
     >
-      <body className='bg-linear-to-br from-stone-950 via-slate-600 to-stone-950'>
+      <body>
         <Header />
         {children}
         <Footer />
