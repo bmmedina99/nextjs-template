850eae80420d06f9ffa9aacd70eb233b303256b6
bmmedina99 <99305658+bmmedina99@users.noreply.github.com>
2026-04-14 23:19:10 +0200

    refactor(404): update not-found design

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -4,32 +4,34 @@ import Main from '@/components/layout/main'
 
 export default function notFound() {
   return (
-    <Main className='flex items-center justify-center grow overflow-hidden'>
-      <div className='relative z-10 max-w-4xl mx-auto space-y-4 text-center'>
-        <p className='mb-4 text-xs text-gray-300 md:text-sm'>
-          src/app/not-found.tsx
-        </p>
-        <p className='text-6xl font-bold leading-none text-transparent sm:text-8xl bg-clip-text bg-linear-to-t from-brandels-100 via-aqua-100 to-brandels-100'>
-          404
-        </p>
-        <section className='space-y-6'>
-          <h1 className='font-bold text-white'>Página no encontrada</h1>
-          <p className='max-w-2xl mx-auto text-xl leading-relaxed text-slate-300 text-pretty'>
-            Lo sentimos, la página que estás buscando no existe o ha sido
-            movida. Pero no te preocupes, hay muchas otras cosas increíbles que
-            puedes explorar.
+    <Main className='pt-16'>
+      <section className='container grid w-full grid-cols-1 gap-16 mx-auto items-centers lg:grid-cols-2'>
+        <div className='flex flex-col justify-center space-y-4'>
+          <p className='mb-4 text-xs text-gray-300 md:text-sm'>
+            src/app/not-found.tsx
           </p>
-        </section>
-        <div className='flex items-center justify-center'>
-          <button
-            type='button'
-            onClick={() => window.history.back()}
-            className='flex gap-4 px-4 py-3 text-white border-0 rounded-md bg-linear-to-r from-blue-500 to-purple-600 hover:from-blue-600 hover:to-purple-700'
+          <h1 className='font-bold text-white'>Página no encontrada</h1>
+          <div className='max-w-xl md:text-xl text-pretty'>
+            <p className='max-w-2xl mx-auto text-xl leading-relaxed text-slate-300 text-pretty'>
+              Lo sentimos, la página que estás buscando no existe o ha sido
+              movida. Pero no te preocupes, hay muchas otras cosas increíbles
+              que puedes explorar.
+            </p>
+          </div>
+          <nav
+            className='flex flex-wrap'
+            aria-label='Acciones'
           >
-            Volver a la página anterior
-          </button>
+            <button
+              type='button'
+              onClick={() => window.history.back()}
+              className='flex gap-4 px-4 py-3 text-white border-0 rounded-md bg-linear-to-r from-blue-500 to-purple-600 hover:from-blue-600 hover:to-purple-700'
+            >
+              Volver a la página anterior
+            </button>
+          </nav>
         </div>
-      </div>
+      </section>
     </Main>
   )
 }
